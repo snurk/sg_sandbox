71da806c656c1926a9c4eab14b14b0e2782f6c09
Sergey Nurk <sergeynurk@gmail.com>
2020-11-15 13:32:56 -0500

    improved script for analyzing intermediate graphs

--- src/chm13_analysis/get_intermediate.sh
+++ src/chm13_analysis/get_intermediate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-scripts_root=$(dirname $(readlink -e $0))/..
+#scripts_root=$(dirname $(readlink -e $0))/..
+scripts_root=~/git/sg_sandbox/src
 
-for g in simplified*000.gfa simplified.final_*.gfa ; do
+#for g in simplified*000.gfa simplified.final_*.gfa ; do
+for g in simplified*000.gfa ; do
     name=$(basename $g .gfa)
     d=$name
 
@@ -13,12 +15,15 @@ for g in simplified*000.gfa simplified.final_*.gfa ; do
 
     $scripts_root/assign_coverage.py $d/resolved_mapping.txt min_read.cov > $d/simplified.cov
 
-    #awk '/^S/{print ">"$2"\n"$3}' simplified.gfa | fold > simplified.nodes.fasta
+    $scripts_root/inject_coverage.py $g $d/simplified.cov > $d/simplified.gfa
 
-    cp $g $d/no_cov.gfa
-    $scripts_root/inject_coverage.py $d/no_cov.gfa $d/simplified.cov > $d/simplified.gfa
-    rm $d/no_cov.gfa
+    awk '/^S/{print ">"$2"\n"$3}' $d/simplified.gfa | fold > $d/simplified.nodes.fasta
 
     echo -e "H\tVN:Z:1.0" > $d/simplified.noseq.gfa
     $scripts_root/../gfacpp/gfatools/gfatools view -S $d/simplified.gfa >> $d/simplified.noseq.gfa
+
+    cd $d
+    $scripts_root/chm13_analysis/mashmap.sh simplified.nodes.fasta &> mashmap.log
+    $scripts_root/chm13_analysis/chr_analysis.sh &> chr_analysis.log
+    cd -
 done
