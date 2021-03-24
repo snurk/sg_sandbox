9a06ec40f5c6e223df878c5fe918e42c22129d5a
Sergey Nurk <sergeynurk@gmail.com>
2021-03-24 18:23:30 -0400

    report non-symmetric overlap sizes

--- src/preprocess_gfa.py
+++ src/preprocess_gfa.py
@@ -11,22 +11,27 @@ def inverse_sign(s):
         assert(false)
 
 def alt_enc(split):
-    return "%s\t%s\t%s\t%s" % (split[2], inverse_sign(split[3]), split[0], inverse_sign(split[1]))
+    return "%s %s %s %s" % (split[2], inverse_sign(split[3]), split[0], inverse_sign(split[1]))
 
 if len(sys.argv) < 2:
     print("Usage: %s <in gfa>" % sys.argv[0])
     print("Deduplicates copies of the same links. Outputs to stdout")
     sys.exit(1)
 
-used = set()
+used = dict()
 with open(sys.argv[1], "r") as gfa:
     for l in gfa:
         if l.startswith("L"):
-            split_col = l.split("\t")[1:5]
-            if "\t".join(split_col) not in used:
+            split_col = l.split("\t")[1:6]
+            link_str = " ".join(split_col[0:4])
+            ovl = int(split_col[4][:-1])
+            if link_str not in used:
                 print(l.rstrip())
-            used.add(alt_enc(split_col))
+            else:
+                if used[link_str] != ovl:
+                    print("For", link_str, "inconsistent overlaps:", used[link_str], "and", ovl, ". Diff", abs(ovl - used[link_str]), file=sys.stderr)
+            used[alt_enc(split_col)] = ovl
             #bypassing a repeated-lines bug
-            used.add("\t".join(split_col))
+            used[("\t".join(split_col))] = ovl
         else:
             print(l.rstrip())
