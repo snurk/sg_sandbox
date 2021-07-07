b6beb178f2a3fb6e1c8072a99a652ebe07c26c97
Sergey Nurk <sergeynurk@gmail.com>
2021-07-07 01:20:16 -0400

    improved script for processing manually simplified graphs

--- src/chm13_analysis/process_simplified.sh
+++ src/chm13_analysis/process_simplified.sh
@@ -2,28 +2,31 @@
 set -eou
 
 if [ "$#" -lt 4 ]; then
-    echo "Usage: $(basename $0) <processed.gfa> <init.gfa> <node_composition.txt> <read_coverage.txt>"
+    echo "Usage: $(basename $0) <simplified.gfa> <init.gfa> <node_composition.txt> <read_coverage.txt> [compact_prefix=m_]"
     exit 1
+fi
+
+prefix="m_"
+if [ "$#" -gt 4 ]; then
+    prefix=$5
 fi
 
 scripts_root=$(dirname $(readlink -e $0))/..
 
-processed=$1
-name=$(basename $processed .gfa)
+simplified=$1
+name=$(basename $simplified .gfa)
 init=$2
 resolved_mapping=$3
 read_cov=$4
-#resolved_mapping=../../resolved_mapping.txt
-#read_cov=../../min_read.cov
 
-grep "^S" $processed | awk '{print $2}' | sed 's/^/S\\s/g' | sed 's/$/\\s/g' > $name.tmp
-head -1 $init > $name.fixed.gfa
-grep -f $name.tmp $init >> $name.fixed.gfa
+grep "^S" $simplified | cut -f 2 > $name.tmp
+echo -e "H\tVN:Z:1.0" > $name.fixed.gfa
+$scripts_root/../tangle-resolution/scripts/extract_subgraph.py $name.tmp < $init | grep "^S" | sed $'s/\tRC.*//g' >> $name.fixed.gfa
 #grep -f $name.tmp $init | sed $'s/\tRC.*//g' >> $name.fixed.gfa
-grep "^L" $processed >> $name.fixed.gfa
+grep "^L" $simplified >> $name.fixed.gfa
 #rm -f $name.tmp
 
-$scripts_root/compact_gfa.py $name.fixed.gfa $name.tmp.gfa 2> $name.final_compress_mapping.txt
+$scripts_root/compact_gfa.py $name.fixed.gfa $name.tmp.gfa $prefix 2> $name.final_compress_mapping.txt
 
 cat $resolved_mapping $name.final_compress_mapping.txt > $name.mapping.txt
 $scripts_root/resolve_layouts.py $name.tmp.gfa $name.mapping.txt > $name.resolved_mapping.txt
