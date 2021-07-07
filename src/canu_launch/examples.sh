7d5895a90d8200b3b31782e752153f687bed44d6
Sergey Nurk <sergeynurk@gmail.com>
2021-07-07 15:45:21 -0400

    explicit path variable

--- src/canu_launch/examples.sh
+++ src/canu_launch/examples.sh
@@ -2,7 +2,8 @@
 set -eou
 
 base_path=$(dirname $(readlink -e $0))
-export CANU_BIN=$(readlink -e ~/git/canu/build/bin)
+canu_path=~/git/canu/build/bin
+export CANU_BIN=$(readlink -e $canu_path)
 
 #example 1 -- single read file
 $base_path/canu.sh $CANU_BIN/canu out_canu reads.fa.gz onSuccess="$base_path/on_init_complete.sh"
