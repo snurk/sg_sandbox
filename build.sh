abebacac8d1f33d810bc16d65742365332fcb21b
Sergey Nurk <sergeynurk@gmail.com>
2021-07-14 17:42:59 -0400

    trivial script to build submodules

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eou
+
+path=$(dirname $(readlink -e $0))
+
+echo "Fetching submodules"
+cd $path
+git submodule update --init --recursive
+cd -
+
+echo "Building Canu"
+cd $path/canu/src
+make $@
+cd -
+
+echo "Building miniasm"
+cd $path/miniasm
+make $@
+cd -
+
+echo "Building miniasm"
+cd $path/miniasm
+make $@
+cd -
+
+echo "Building gfacpp tool collection"
+cd $path/gfacpp
+make $@
+cd -
