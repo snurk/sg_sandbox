99b2c8a727db547532c7e9d1258ff2f6d2466bc5
Sergey Nurk <sergeynurk@gmail.com>
2021-08-03 00:38:37 -0400

    better variable name

--- src/consensus/resolve_layouts.sh
+++ src/consensus/resolve_layouts.sh
@@ -8,13 +8,13 @@ fi
 
 layout=$1
 mapping=$2
-path=$3
-mkdir -p $(dirname $path)
+out_path=$3
+mkdir -p $(dirname $out_path)
 
-awk '{print $1}' $layout > $path.names
-cat $layout $mapping > $path.full_mapping
+awk '{print $1}' $layout > $out_path.names
+cat $layout $mapping > $out_path.full_mapping
 
-$(dirname $(readlink -e $0))/../resolve_layouts.py $path.names $path.full_mapping > $path
+$(dirname $(readlink -e $0))/../resolve_layouts.py --resolved-marker _i $out_path.names $out_path.full_mapping > $out_path
 
-rm $path.names
-rm $path.full_mapping
+rm $out_path.names
+rm $out_path.full_mapping
