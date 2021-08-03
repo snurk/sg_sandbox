f0f23d4e74c6f4104cf0552e2b8f96811dce6db9
Sergey Koren <sergek@umd.edu>
2021-08-03 11:44:55 -0400

    Add read length option

--- src/canu_launch/canu.sh
+++ src/canu_launch/canu.sh
@@ -8,7 +8,8 @@ fi
 
 canu_exec=$1
 out_dir=$2
-reads=$3
+max_len=$3
+reads=$4
 
 #Options to consider
 #OvlMerSize=31 (default is 22)
@@ -16,4 +17,4 @@ reads=$3
 echo "Assembling reads $reads with Canu from $canu_exec into $out_dir"
 echo "Additional parameters ${@:4}"
 
-$canu_exec -p asm -d $out_dir -assemble canuIterationMax=1 maxInputCoverage=10000 batMemory=200 genomeSize='3.1g' gridOptionsUTGOVL='--time=12:00:00' gridOptionsmeryl='--mem-per-cpu=4g' gridOptions='--time=24:00:00' ovlThreads=32 utgOvlRefBlockLength=10000000000 ovlMerThreshold=20000 minReadLength=4000 minOverlapLength=2000 batOptions="-covgaptype uncovered -D symmetricoverlaps -stop bestedges -ef 1e-5" stopAfter=unitig -pacbio-hifi $reads "${@:4}"
+$canu_exec -p asm -d $out_dir -assemble canuIterationMax=1 maxInputCoverage=10000 batMemory=200 genomeSize='3.1g' gridOptionsUTGOVL='--time=12:00:00' gridOptionsmeryl='--mem-per-cpu=4g' gridOptions='--time=24:00:00' ovlThreads=32 utgOvlRefBlockLength=10000000000 ovlMerThreshold=20000 minReadLength=4000 minOverlapLength=2000 batOptions="-covgaptype uncovered -D symmetricoverlaps -stop bestedges -ef 1e-5 -readlen 4000-$max_len" stopAfter=unitig -pacbio-hifi $reads "${@:4}"
--- src/canu_launch/examples.sh
+++ src/canu_launch/examples.sh
@@ -2,11 +2,11 @@
 set -eou
 
 base_path=$(dirname $(readlink -e $0))
-canu_path=~/git/canu/build/bin
+canu_path=/home/nurks2/git/canu/build/bin
 export CANU_BIN=$(readlink -e $canu_path)
 
 #example 1 -- single read file
-$base_path/canu.sh $CANU_BIN/canu out_canu reads.fa.gz onSuccess="$base_path/on_init_complete.sh"
+$base_path/canu.sh $CANU_BIN/canu out_canu 18000 reads.fa.gz onSuccess="$base_path/on_init_complete.sh"
 
 #example 2 -- multiple read files
-$base_path/canu.sh $CANU_BIN/canu out_canu reads1.fastq.gz -pacbio-hifi reads2.fastq.gz -pacbio-hifi reads3.fastq.gz onSuccess="$base_path/on_init_complete.sh"
+$base_path/canu.sh $CANU_BIN/canu out_canu 18000 reads1.fastq.gz -pacbio-hifi reads2.fastq.gz -pacbio-hifi reads3.fastq.gz onSuccess="$base_path/on_init_complete.sh"
