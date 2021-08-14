fea5982b5b301a84eee3ba840f0a199be22087a1
Sergey Nurk <sergeynurk@gmail.com>
2021-08-14 14:53:00 -0400

    update read names in layout

---
+++ src/consensus/translate_backbone_layout.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import re
+from functools import partial
+
+def read_map(fn):
+    d = dict()
+    with open(fn, "r") as f:
+        for l in f:
+            split = l.split()
+            d['read'+split[0]] = ('read'+split[1])
+    #return dict([(l.split()[1], l.split()[0]) for l in open(fn, "r")])
+    return d
+
+if len(sys.argv) < 2:
+    print("Usage: %s <name map (from -> to) file>.\nReads layout from standard input and prints converted layout to standard output." % sys.argv[0])
+    sys.exit(1)
+
+mapping = read_map(sys.argv[1])
+
+for line in sys.stdin:
+    fields = line.strip().split()
+    print(fields[0], ','.join([(mapping[t[:-1]] + t[-1]) for t in fields[1].strip().split(',')]))
