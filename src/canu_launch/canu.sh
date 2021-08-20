76b995148f7f61c13e964ef8ec6b9f3b852ed2aa
Sergey Koren <sergek@umd.edu>
2021-08-20 15:45:51 -0400

    Fix duplication of first read input

--- src/canu_launch/canu.sh
+++ src/canu_launch/canu.sh
@@ -15,6 +15,6 @@ reads=$4
 #OvlMerSize=31 (default is 22)
 
 echo "Assembling reads $reads with Canu from $canu_exec into $out_dir"
-echo "Additional parameters ${@:4}"
+echo "Additional parameters ${@:5}"
 
-$canu_exec -p asm -d $out_dir -assemble canuIterationMax=1 maxInputCoverage=10000 batMemory=200 genomeSize='3.1g' gridOptionsUTGOVL='--time=12:00:00' gridOptionsmeryl='--mem-per-cpu=4g' gridOptions='--time=24:00:00' ovlThreads=32 utgOvlRefBlockLength=10000000000 ovlMerThreshold=20000 minReadLength=4000 minOverlapLength=2000 batOptions="-covgaptype uncovered -D symmetricoverlaps -stop bestedges -ef 1e-5 -readlen 4000-$max_len" stopAfter=unitig -pacbio-hifi $reads "${@:4}"
+$canu_exec -p asm -d $out_dir -assemble canuIterationMax=1 maxInputCoverage=10000 batMemory=200 genomeSize='3.1g' gridOptionsUTGOVL='--time=12:00:00' gridOptionsmeryl='--mem-per-cpu=4g' gridOptions='--time=24:00:00' ovlThreads=32 utgOvlRefBlockLength=10000000000 ovlMerThreshold=20000 minReadLength=4000 minOverlapLength=2000 batOptions="-covgaptype uncovered -D symmetricoverlaps -stop bestedges -ef 1e-5 -readlen 4000-$max_len" stopAfter=unitig -pacbio-hifi $reads "${@:5}"
