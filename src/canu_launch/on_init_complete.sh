34bede743aa871d503974d9401d68cc5026ff42d
Sergey Koren <sergek@umd.edu>
2021-08-22 10:32:24 -0400

    Properly pass through max read length param

--- src/canu_launch/on_init_complete.sh
+++ src/canu_launch/on_init_complete.sh
@@ -7,6 +7,7 @@ if [ "$#" -lt 1 ]; then
 fi
 
 name=$1
+max_read_len=`cat max_read_len`
 launch_path=$(pwd)
 oea_run_path=${launch_path}_oea
 
@@ -14,5 +15,5 @@ if [ ! -f oea_corrected.fa.gz ] ; then
     sbatch -W --cpus-per-task=8 --mem=20g --time=8:00:00 --partition=norm $(dirname $0)/fix_errors.sh $CANU_BIN/fixErrors ./ oea_corrected.fa.gz
 fi
 
-$(dirname $0)/canu.sh $CANU_BIN/canu $oea_run_path ./oea_corrected.fa.gz
+$(dirname $0)/canu.sh $CANU_BIN/canu $oea_run_path $max_read_len ./oea_corrected.fa.gz
 #onSuccess=$(dirname $0)/on_oea_run_complete.sh
