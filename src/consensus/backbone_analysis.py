e6f4314bda994305d0a0faf4a09bb44090662b3a
Sergey Nurk <sergeynurk@gmail.com>
2021-08-26 20:43:40 -0400

    fixes in backbone splitting logic

--- src/consensus/backbone_analysis.py
+++ src/consensus/backbone_analysis.py
@@ -15,6 +15,9 @@ def swap(c):
         return '-'
     assert False
 
+def swap_segment(s):
+    return s[:-1] + swap(s[-1])
+
 def split_segment_desc(s_d):
     return (s_d[:-1], s_d[-1])
 
@@ -26,19 +29,11 @@ def transform_dir_seg(s):
     assert(false)
 
 parser = argparse.ArgumentParser(description="Join sequences of GFA nodes")
-parser.add_argument("paths", help="Part of GAF detailing paths (name id1[+-],id2[+-],... start end)")
+parser.add_argument("paths", help="Part of GAF detailing paths (name id1[+-],id2[+-],...)")
 parser.add_argument("coverage", help="Node coverage file")
 parser.add_argument("composition", help="Node composition stats")
 #parser.add_argument("result", help="Output file")
 args = parser.parse_args()
-
-#if len(sys.argv) < 4:
-#    print("Usage: script.py <contigs> <order_fn, with lines id1+,id2-,...> <gfa> <output fasta>")
-#    exit(1)
-
-#contigs_fn = sys.argv[1]
-#order = sys.argv[2]
-#out_fn = sys.argv[4]
 
 seg_cov=dict()
 with open(args.coverage, 'r') as f:
@@ -61,82 +56,44 @@ def match_length(a, i, j):
         k += 1
     return k
 
-def reuse_stats(segment_descs, excess_coeff, expected_cov):
+def reuse_stats(segment_descs):
+    #FIXME Parameterize!
+    #READ_EXCESS_COEFF = 2
+    READ_EXCESS_COEFF = 1
+    EXPECTED_COV = 15.
+
     reused = list()
-    bag = defaultdict(int)
+    seg_mults = defaultdict(int)
     with_orientation = set()
     for s in segment_descs:
         assert s[:-1] in seg_cov
         assert s[:-1] in seg_comp
-        if s[:-1] in bag:
+        if s[:-1] in seg_mults:
             if not s in with_orientation:
                 print("Trouble segment %s in the path in different orientations" % s[:-1], file=sys.stderr)
         #        assert False
 
-        bag[s[:-1]] += 1
+        seg_mults[s[:-1]] += 1
         with_orientation.add(s)
 
-    for b in bag:
-        if bag[b] > 1:
-            print("Reuse of node %s (cov: %f, #reads: %d) %d times" % (b, seg_cov[b], seg_comp[b], bag[b]), file=sys.stderr)
-            if seg_cov[b] < expected_cov * bag[b]:
-                print("WARN", file=sys.stderr)
-            if excess_coeff * bag[b] > seg_comp[b]:
+    for b in seg_mults:
+        if seg_mults[b] > 1:
+            if seg_cov[b] < EXPECTED_COV * seg_mults[b]:
+                print("WARN: Node %s (cov: %f, #reads: %d) used %d times" % (b, seg_cov[b], seg_comp[b], seg_mults[b]), file=sys.stderr)
+
+            #if seg_mults[b] > seg_comp[b] or (seg_mults[b] > 2 and (READ_EXCESS_COEFF * seg_mults[b]) > seg_comp[b]):
+            if seg_comp[b] < READ_EXCESS_COEFF * seg_mults[b]:
+                print("Reuse of node %s (cov: %f, #reads: %d) %d times" % (b, seg_cov[b], seg_comp[b], seg_mults[b]), file=sys.stderr)
                 reused.append(b)
             else:
-                print("No need to split at node %s: #reads %d, reused %d times" % (b, seg_comp[b], bag[b]), file=sys.stderr)
+                print("No need to split at node %s: #reads %d, reused %d times" % (b, seg_comp[b], seg_mults[b]), file=sys.stderr)
 
     return reused
 
 def analyze_path(segment_descs):
-    READ_EXCESS_COEFF = 2
-    EXPECTED_COV = 15.
-    reused = reuse_stats(segment_descs, READ_EXCESS_COEFF, EXPECTED_COV)
+    reused = reuse_stats(segment_descs)
     return [i for i in range(0, len(segment_descs)) if segment_descs[i][:-1] in reused]
-    #i = 0
-    #n = len(segment_descs)
-    #last_printed = 0
-    #regions = list()
-    #while i < n:
-    #    s = segment_descs[i]
-    #    j = 0
-    #    minmax = 1000
-    #    while j < i:
-    #        s2 = segment_descs[j]
-    #        if s[:-1] == s2[:-1]:
-    #            if s[-1] != s2[-1]:
-    #                print("Trouble segment %s in the path in different orientations (positions %d and %d)" % (s[:-1], i, j), file=sys.stderr)
-    #                assert False
-    #            else:
-    #                print("Considering match %s between (positions %d and %d)" % (s, i, j), file=sys.stderr)
-    #            m = match_length(segment_descs, i, j)
-    #            print("Match length was %d" % m, file=sys.stderr)
-    #            if minmax > m:
-    #                print("Shorter than previous maximal match", file=sys.stderr)
-    #                minmax = m
-    #        j += 1
 
-    #    if minmax != 1000:
-    #        if i > last_printed:
-    #            print("region %s" % ','.join(segment_descs[last_printed: i]), file=sys.stderr)
-    #            regions.append(i)
-    #        print("Will jump ahead %d positions" % minmax, file=sys.stderr)
-    #        print("repetitive region %s" % ','.join(segment_descs[i: i + minmax]), file=sys.stderr)
-    #        regions.append(i + minmax)
-    #        last_printed = i + minmax
-
-    #        i += minmax
-    #    else:
-    #        i += 1
-
-    #if last_printed < n:
-    #    print("region %s" % ','.join(segment_descs[last_printed: n]), file=sys.stderr)
-    #    regions.append(n)
-    #print("======================================", file=sys.stderr)
-
-    #return regions
-
-#with open(args.result, 'w') as out:
 for line in open(args.paths, 'r'):
     print('=======================================', file=sys.stderr)
     s = line.split()
@@ -153,29 +110,35 @@ for line in open(args.paths, 'r'):
     else:
         segment_descs = [t.strip() for t in l.split(',')]
 
-    regions = analyze_path(segment_descs)
-    regions.append(len(segment_descs))
-    block = list()
-    block_end = 0
+    boundaries = analyze_path(segment_descs)
+    boundaries.append(len(segment_descs))
+
+    curr_block = list()
+    curr_block_end = 0
     block_cnt = 1
-    for boundary in regions:
-        add = True
-        for i in range(block_end, boundary):
-            if segment_descs[i] in block:
-                add = False
-                break
-
-        if add:
-            block.extend(segment_descs[block_end: boundary])
+    for boundary in boundaries:
+        chunk = segment_descs[curr_block_end : boundary]
+        print('Considering addition of chunk [%s] to the current block [%s]' % (','.join(chunk), ','.join(curr_block)), file=sys.stderr)
+        #add = True
+        #for i in range(curr_block_end, boundary):
+        #    if segment_descs[i] in curr_block:
+        #        add = False
+        #        break
+        if segment_descs[curr_block_end] not in curr_block and swap_segment(segment_descs[curr_block_end]) not in curr_block:
+            print('Will extend', file=sys.stderr)
+            curr_block.extend(chunk)
         else:
-            assert block_end > 0
-            print('%s.%d %s' % (s[0], block_cnt, ','.join(block)))
+            print('Can not extend. Reporting current block as [%s.%d %s]' % (s[0], block_cnt, ','.join(curr_block)), file=sys.stderr)
+            assert curr_block_end > 0
+            print('%s.%d %s' % (s[0], block_cnt, ','.join(curr_block)))
             block_cnt += 1
-            block = segment_descs[block_end: boundary]
+            print('Resetting current block to', ','.join(chunk), file=sys.stderr)
+            curr_block = chunk
 
-        block_end = boundary
+        curr_block_end = boundary
+        print('Updating block boundary to', curr_block_end, file=sys.stderr)
 
     if block_cnt == 1:
-        print('%s %s' % (s[0], ','.join(block)))
+        print('%s %s' % (s[0], ','.join(curr_block)))
     else:
-        print('%s.%d %s' % (s[0], block_cnt, ','.join(block)))
+        print('%s.%d %s' % (s[0], block_cnt, ','.join(curr_block)))
