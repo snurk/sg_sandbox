c751876bfb0643a7032f01f77ca3c4afd6764c8a
Sergey Nurk <sergeynurk@gmail.com>
2021-09-09 11:10:31 -0400

    logging improvement; no tmp file

--- src/build_graph.sh
+++ src/build_graph.sh
@@ -27,7 +27,8 @@ echo -e "H\tVN:Z:1.0" > microasm.gfa
 $scripts_root/../miniasm/miniasm -e 0 -d 0 -n 0 -m $min_ovl -s $min_ovl -f $reads $ovl_paf >> microasm.gfa 2> microasm.log
 
 # Deduplicate copies of same links.
+# + Trick to make gfapy process the custom tag
 echo "Deduplicating links"
-$scripts_root/preprocess_gfa.py microasm.gfa > processed.gfa
-#Trick to make gfapy process the custom tag
-sed -i 's/SD/sd/g' processed.gfa
+$scripts_root/preprocess_gfa.py microasm.gfa | sed 's/SD/sd/g' > processed.gfa
+
+echo "Microasm construction complete"
