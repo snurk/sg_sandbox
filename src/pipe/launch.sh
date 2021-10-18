c178a845d6e1817d75b90d209cc306a1cb4f8f93
Sergey Nurk <sergeynurk@gmail.com>
2021-10-18 17:02:36 -0400

    launch script usability fixes

--- src/pipe/launch.sh
+++ src/pipe/launch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -eou
 
-if [ "$#" -lt 1 ]; then
+if [ "$#" -lt 2 ]; then
     echo "script.sh <config.yaml> <out_dir> [additional snakemake arguments]*"
-    exit
+    exit 239
 fi
 
-dir=$(readlink -e $2)
 config=$(readlink -e $1)
+mkdir -p $2
+dir=$(readlink -e $2)
 
 cd $(dirname $0)
 
@@ -15,6 +16,9 @@ export SCRIPT_PATH=$(readlink -e .)
 
 # -o /home/snurk/logs/ -e /home/snurk/logs/
 echo "Will use config file: \"$config\""
+echo "Output folder: \"$dir\""
+echo "Additional snakemake arguments: \"${@:3}\""
+
 snakemake --latency-wait 60 -j 60 --local-cores 4 --cluster-config cluster.json --configfile $config --cluster "sbatch --ntasks 1 --mem {cluster.mem}G --cpus-per-task {cluster.n} --time {cluster.time}" --directory $dir "${@:3}"
 
 cd -
--- src/pipe/launch_local.sh
+++ src/pipe/launch_local.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 set -eou
 
-if [ "$#" -lt 1 ]; then
-    echo "script.sh <with config.yaml> <out_dir> [additional snakemake arguments]*"
-    exit
+if [ "$#" -lt 2 ]; then
+    echo "script.sh <config.yaml> <out_dir> [additional snakemake arguments]*"
+    exit 239
 fi
 
 config=$(readlink -e $1)
+mkdir -p $2
 dir=$(readlink -e $2)
 
 cd $(dirname $0)
 
 export SCRIPT_PATH=$(readlink -e .)
+
+echo "Will use config file: \"$config\""
+echo "Output folder: \"$dir\""
+echo "Additional snakemake arguments: \"${@:3}\""
 
 snakemake --latency-wait 60 -j 8 --configfile $config --directory $dir "${@:3}"
 
