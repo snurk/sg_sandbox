cad5b356d01aca7e345f45fa53ecce6be21ef8f5
Sergey Nurk <sergeynurk@gmail.com>
2021-10-22 15:52:51 -0400

    script running make clean in submodules

---
+++ clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eou
+
+path=$(dirname $(readlink -e $0))
+
+echo "Building Canu"
+cd $path/canu/src
+make clean
+cd -
+
+echo "Building miniasm"
+cd $path/miniasm
+make clean
+cd -
+
+echo "Building gfacpp tool collection"
+cd $path/gfacpp
+make clean
+cd -
+
+echo "Building GraphAligner"
+cd $path/GraphAligner
+make clean
+cd -
+
